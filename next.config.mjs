3f81ad5bfad3d8d7268babfbfaac97fa1bb2a325
rifat-mahmudul <mahmudulhasanrifat094@gmail.com>
2025-06-04 16:57:20 +0600

    olive's functionality done

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'flagcdn.com',
-                pathname: '/**', // Allows all paths under flagcdn.com
-            }
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "flagcdn.com",
+        pathname: "/**", // Allows all paths under flagcdn.com
+      },
+      {
+        protocol: "https",
+        hostname: "flagsapi.com",
+        pathname: "/**", // Allows all paths under flagcdn.com
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/(website)/search-result/page.tsx
+++ src/app/(website)/search-result/page.tsx
@@ -1,11 +1,14 @@
-import SearchResult from "@/components/SearchResult/SearchResult"
+import SearchResult from "@/components/SearchResult/SearchResult";
+import { Suspense } from "react";
 
 const page = () => {
   return (
     <div className="mt-10 mb-16">
-       <SearchResult /> 
+      <Suspense>
+        <SearchResult />
+      </Suspense>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
