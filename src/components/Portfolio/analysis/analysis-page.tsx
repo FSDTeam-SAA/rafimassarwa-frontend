ce5e6a5a6b7922efedb69105c67eda33bb3102ff
undefinedSahed <sahedrahmanshakil@gmail.com>
2025-06-28 18:53:43 +0600

    analysis page integration

--- src/components/Portfolio/analysis/analysis-page.tsx
+++ src/components/Portfolio/analysis/analysis-page.tsx
@@ -6,7 +6,7 @@ import PerformanceGrid from "./performance-grid"
 export default function AnalysisPage() {
     return (
         <div className="">
-            <div className="grid grid-cols-1 md:w-[98svw] lg:w-auto lg:px-3 lg:grid-cols-3 gap-6 ">
+            <div className="grid grid-cols-1 w-[95vw] md:w-[98svw] lg:w-auto lg:px-3 lg:grid-cols-3 gap-6">
                 <div className="col-span-1 lg:w-[500px] w-full">
                     <h3 className="text-xl font-semibold pb-4">Asset Allocation</h3>
                     <div className="min-w-full py-1">
