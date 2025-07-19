aeb6ca57dc02f95c43e2b82919244a46f5307fe7
rifat-mahmudul <mahmudulhasanrifat094@gmail.com>
2025-07-19 16:02:23 +0600

    all stock modify

--- src/components/HomePage/AllStock.tsx
+++ src/components/HomePage/AllStock.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useUserPayment } from "../context/paymentContext";
 
 interface TrendingStock {
   symbol: string;
@@ -20,6 +21,10 @@ interface StockTrackingTableProps {
 export default function StockTrackingTable({
   trendingStocks,
 }: StockTrackingTableProps) {
+  console.log(trendingStocks);
+
+  const { paymentType } = useUserPayment();
+
   if (!trendingStocks || trendingStocks.length === 0) {
     return (
       <div className="mt-4 p-8 text-center text-gray-500">
@@ -29,128 +34,112 @@ export default function StockTrackingTable({
   }
 
   return (
-    <div className="w-full overflow-scroll scrollbar-hide ">
-      <div className="min-w-[700px] lg:max-w-4xl mx-auto">
-        <table className="w-full">
-          <thead>
-            <tr className="mt-4 grid grid-cols-4 gap-2 rounded-t-md bg-green-50 p-2 text-sm font-medium">
-              <th className="text-left">Company</th>
-              <th className="text-left">Smart Score</th>
-              <th className="text-left">Price & Change</th>
-              <th className="text-left">
-                Rating in Last 30 <br /> Days
-              </th>
-            </tr>
-          </thead>
-          <tbody className="bg-white rounded-b-md">
-            {trendingStocks.map((stock, index) => {
-              const totalRatings = stock.buy + stock.hold + stock.sell;
-              const smartScore =
-                totalRatings > 0
-                  ? Math.round((stock.buy / totalRatings) * 100)
-                  : 0;
-              const isNegative =
-                stock.percentChange !== null && stock.percentChange < 0;
+    <div className="w-full overflow-x-auto">
+      <div className="min-w-[600px] w-full max-w-4xl mx-auto">
+        <div className="mt-4 grid grid-cols-4 gap-2 rounded-t-md bg-green-50 p-2 text-sm font-medium">
+          <div>Stock Info</div>
+          <div className="ml-10">Price & Changes</div>
+          <div className="ml-7">Price</div>
+          <div>
+            Upside <br /> Potential
+          </div>
+        </div>
 
-              return (
-                <tr
-                  key={`${stock.symbol}-${index}`}
-                  className={`grid grid-cols-4 py-3 items-center ${
-                    index !== trendingStocks.length - 1
-                      ? "border-b border-gray-200"
-                      : ""
-                  }`}
-                >
-                  {/* Company */}
-                  <td>
-                    <Link href={`/search-result?q=${stock?.symbol}`}>
-                      <div>
-                        <div className="text-[#2e7d32] font-medium">
-                          {stock.symbol}
-                        </div>
-                        <div className="text-gray-500 text-sm">
-                          Stock Symbol
-                        </div>
-                      </div>
-                    </Link>
-                  </td>
-
-                  {/* Smart Score */}
-                  <td className="font-medium">
-                    <p className="ml-16">{stock.currentPrice || "N/A"}</p>
-                    <div className="flex items-center gap-1">
-                      {isNegative && (
-                        <Image
-                          src="/images/downarrow.png"
-                          alt="downarrow"
-                          width={15}
-                          height={15}
-                        />
-                      )}
-                      <span
-                        className={
-                          isNegative ? "text-red-500" : "text-green-500"
-                        }
-                      >
-                        {stock.priceChange !== null &&
-                        stock.percentChange !== null
-                          ? `${stock.priceChange.toFixed(
-                              2
-                            )} (${stock.percentChange.toFixed(2)}%)`
-                          : "N/A"}
-                      </span>
-                    </div>
-                  </td>
-
-                  {/* Price & Change */}
-                  <td>
-                    <div className="flex items-center ml-8">
-                      <div
-                        className="relative w-10 h-10 rounded-full flex items-center justify-center bg-[#28A745]"
-                        style={{
-                          filter: "blur(3px)",
-                          boxShadow: "inset 0 0 5px rgba(0, 0, 0, 0.3)",
-                        }}
-                      >
-                        <Image
-                          src="/images/lock.png"
-                          alt="lock-image"
-                          width={20}
-                          height={20}
-                          className="absolute"
-                        />
+        <div className="bg-white rounded-b-md">
+          {trendingStocks.map((stock, index) => {
+            return (
+              <div
+                key={`${stock.symbol}-${index}`}
+                className={`grid grid-cols-4 p-3 items-center ${
+                  index !== trendingStocks.length - 1
+                    ? "border-b border-gray-200"
+                    : ""
+                }`}
+              >
+                {/* Stock Info */}
+                <div className="flex items-center">
+                  <Link href={`/search-result?q=${stock?.symbol}`}>
+                    {" "}
+                    <div>
+                      <div className="text-[#2e7d32] font-medium">
+                        {stock.symbol}
                       </div>
                     </div>
-                  </td>
+                  </Link>
+                </div>
 
-                  {/* Rating */}
-                  <td>
-                    <div className="flex gap-3">
-                      <div className="w-16 h-16 rounded-full border-4 border-yellow-400 flex items-center justify-center">
-                        <span className="text-lg font-bold">{smartScore}</span>
-                      </div>
+                {/* Company */}
+                <div className="ml-10">
+                  <div className="text-green-500 font-medium">
+                    ${stock.currentPrice || "N/A"}
+                  </div>
+                  <div className="text-gray-500 text-sm">
+                    {stock.priceChange !== null && stock.percentChange !== null
+                      ? `${
+                          stock.priceChange >= 0 ? "+" : ""
+                        }${stock.priceChange.toFixed(
+                          2
+                        )} (${stock.percentChange.toFixed(2)}%)`
+                      : "No change data"}
+                  </div>
+                </div>
 
-                      <div className="flex flex-col items-end text-sm">
-                        <div className="flex items-center gap-1">
-                          <div className="w-3 h-3 bg-green-500"></div>
-                          <span>{stock.buy} Buy</span>
-                        </div>
-                        <div className="flex items-center gap-1">
-                          <div className="w-3 h-3 bg-yellow-400"></div>
-                          <span>{stock.hold} Hold</span>
-                        </div>
-                        <div className="flex items-center gap-1">
-                          <div className="w-3 h-3 bg-red-500"></div>
-                          <span>{stock.sell} Sell</span>
-                        </div>
-                      </div>
+                {/* Rating & Price Target */}
+                <div className="flex items-center ml-8">
+                  {paymentType === "free" ? (
+                    <div
+                      className="relative w-10 h-10 rounded-full flex items-center justify-center bg-[#28A745] z-0"
+                      style={{
+                        filter: "blur(1px)",
+                        boxShadow: "inset 0 0 5px rgba(0, 0, 0, 0.3)",
+                      }}
+                    >
+                      <Image
+                        src="/images/lock.png"
+                        alt="lock-image"
+                        width={20}
+                        height={20}
+                        className="absolute z-1000"
+                      />
                     </div>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+                  ) : (
+                    <>
+                      <div>${stock?.targetMean?.toFixed(2)}</div>
+                    </>
+                  )}
+                </div>
+
+                {/* Upside Potential */}
+                <div className="text-green-500 font-medium mt-3 ml-3">
+                  {paymentType === "free" ? (
+                    <div
+                      className="relative w-10 h-10 rounded-full flex items-center justify-center bg-[#28A745] z-0"
+                      style={{
+                        filter: "blur(1px)",
+                        boxShadow: "inset 0 0 5px rgba(0, 0, 0, 0.3)",
+                      }}
+                    >
+                      <Image
+                        src="/images/lock.png"
+                        alt="lock-image"
+                        width={20}
+                        height={20}
+                        className="absolute z-1000"
+                      />
+                    </div>
+                  ) : (
+                    <>
+                      {stock.upsidePercent ? `${stock.upsidePercent}%` : "N/A"}
+                      <div className="text-green-500 text-sm">
+                        {stock.upsidePercent ? "Upside" : "No data"}
+                      </div>
+                    </>
+                  )}
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
